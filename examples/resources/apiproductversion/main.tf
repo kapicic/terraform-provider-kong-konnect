e23c06b3746ea64eb13ff29c1eacd44773c8bbb3
Michael Bastos <bastosmichael@gmail.com>
2023-11-08 08:01:14 -0600

    adding docs

--- examples/resources/apiproductversion/main.tf
+++ examples/resources/apiproductversion/main.tf
@@ -1,18 +1,18 @@
 resource "kong_api_product_version" "example" {
-    name = "name"
+  name = "name"
 
-    gateway_service = {
-                id = "id"
-                control_plane_id = "control_plane_id"
-}
+  gateway_service = {
+    id               = "id"
+    control_plane_id = "control_plane_id"
+  }
 
 
-    publish_status = "publish_status"
+  publish_status = "publish_status"
 
-    deprecated = false
+  deprecated = false
 
-    notify = false
+  notify = false
 
-    api_product_id = "api_product_id"
+  api_product_id = "api_product_id"
 
 }
--- examples/resources/service/main.tf
+++ examples/resources/service/main.tf
@@ -1,51 +1,51 @@
 resource "kong_service" "example" {
-    connect_timeout = "connect_timeout"
+  connect_timeout = "connect_timeout"
 
-    created_at = "created_at"
+  created_at = "created_at"
 
-    enabled = false
+  enabled = false
 
-    host = "host"
+  host = "host"
 
+  id = "id"
+
+  name = "name"
+
+  path = "path"
+
+  port = "port"
+
+  protocol = "protocol"
+
+  read_timeout = "read_timeout"
+
+  retries = "retries"
+
+  updated_at = "updated_at"
+
+  write_timeout = "write_timeout"
+
+  ca_certificates = [
+    "ca_certificates"
+  ]
+
+  client_certificate = {
     id = "id"
-
-    name = "name"
-
-    path = "path"
-
-    port = "port"
-
-    protocol = "protocol"
-
-    read_timeout = "read_timeout"
-
-    retries = "retries"
-
-    updated_at = "updated_at"
-
-    write_timeout = "write_timeout"
-
-    ca_certificates = [
-        "ca_certificates"
-    ]
-
-    client_certificate = {
-                id = "id"
-}
+  }
 
 
-    tags = [
-        "tags"
-    ]
+  tags = [
+    "tags"
+  ]
 
-    tls_verify = false
+  tls_verify = false
 
-    tls_verify_depth = "tls_verify_depth"
+  tls_verify_depth = "tls_verify_depth"
 
-    url = "url"
+  url = "url"
 
-    runtime_group_id = "runtime_group_id"
+  runtime_group_id = "runtime_group_id"
 
-    service_id = "service_id"
+  service_id = "service_id"
 
 }
